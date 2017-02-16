9a209fdeb2b588ec1508875be87987748bfae7bb
Rafał Świerkot <rafalswierkot@gmail.com>
2017-02-16 20:33:54 +0100

    Removed unused class.

--- domain/src/main/java/eu/rampsoftware/er/domain/pojo/CurrencyHome.java
+++
@@ -1,9 +0,0 @@
-//package eu.rampsoftware.er.domain.pojo;
-//
-//import eu.rampsoftware.er.data.CurrencyData;
-//
-//public class CurrencyHome {
-//    public CurrencyHome(final CurrencyData currencyData) {
-//
-//    }
-//}
